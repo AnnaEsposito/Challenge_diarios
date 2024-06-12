d0b6c061427e62a98c9954a3471cc0bc2e154491
AnnaEsposito <101672244+AnnaEsposito@users.noreply.github.com>
2024-06-12 10:03:07 -0400

    Add files via upload
    challenge_dia2

---
+++ tabla_multiplicar.py
@@ -0,0 +1,6 @@
+
+numero_ingresado=input("ingrese un numero: ")
+
+for i  in range(10):
+    tabla_de_multiplicar=int(numero_ingresado)*i+int(numero_ingresado)*int(numero_ingresado)
+    print(tabla_de_multiplicar)
